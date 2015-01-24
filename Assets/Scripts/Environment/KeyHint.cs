68b00fb6fb8563da822db35a7e33248433faa9b6
Martin Hodler <martin.hodler@gmail.com>
2015-01-24 02:35:06 +0100

    Added KeyHint Component

---
+++ Assets/Scripts/Environment/KeyHint.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyHint : MonoBehaviour {
+
+    [SerializeField]
+    private string _key = "W";
+
+    [SerializeField]
+    private string _hint = "Something happens if you press this key!";
+
+    [SerializeField]
+    private TextMesh _keyMesh;
+
+    [SerializeField]
+    private TextMesh _hintMesh;
+
+    private List<MeshRenderer> _renderers;
+
+	// Use this for initialization
+	void Start () {
+	    
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MeshRenderer mr = transform.GetChild(i).GetComponent<MeshRenderer>();
+
+            if (mr != null)
+                _renderers.Add(mr);
+        }
+
+        SetVisibility(false);
+	}
+
+    public void Redraw()
+    {
+        _keyMesh.text = _key;
+        _hintMesh.text = _hint;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            SetVisibility(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            SetVisibility(false);
+        }
+    }
+
+    void SetVisibility(bool visible)
+    {
+        for (int i = 0; i < _renderers.Count; i++)
+        {
+            _renderers[i].enabled = visible;
+        }
+    }
+}
