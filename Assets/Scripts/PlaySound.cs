b688ac50976a65075486ca15a40ef28795b43ee1
Martin Hodler <martin.hodler@gmail.com>
2015-01-25 11:44:40 +0100

    Music fader

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -6,9 +6,66 @@ using System.Collections.Generic;
 public class PlaySound : MonoBehaviour {
 
     public List<AudioSource> Sources;
+    private int playingSound = -1;
+    private int nextSound = -1;
+    private float playingVolume = 1f;
+
+    private bool fade = false;
+    private bool fadeIn = true;
 
     public void Play(int index)
     {
         Sources[index].Play();
+    }
+
+    public void PlayFade(int index, float volume = 1f)
+    {
+        if (playingSound == -1)
+        {
+            playingSound = index;
+            fadeIn = true;
+            fade = true;
+
+            playingVolume = volume;
+            Sources[playingSound].volume = 0f;
+            Sources[playingSound].Play();
+        }
+        else
+        {
+            fadeIn = false;
+            playingVolume = volume;
+            nextSound = index;
+            fade = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (fade)
+        {
+            if (fadeIn)
+            {
+                Sources[playingSound].volume = Mathf.Lerp(Sources[playingSound].volume, 0.3f, Time.fixedDeltaTime * 4);
+
+                if (Sources[playingSound].volume == playingVolume)
+                    fade = false;
+            }
+            else
+            {
+                Sources[playingSound].volume = Mathf.Lerp(Sources[playingSound].volume, 0f, Time.fixedDeltaTime * 4);
+
+                if (Sources[playingSound].volume < 0.001f)
+                {
+                    Sources[playingSound].Stop();
+                    playingSound = nextSound;
+
+                    playingVolume = Sources[playingSound].volume;
+                    Sources[playingSound].volume = 0f;
+                    Sources[playingSound].Play();
+
+                    fadeIn = true;
+                }
+            }
+        }
     }
 }
